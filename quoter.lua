f59711a7d03c6d24441bf11f01674cff375d6460
mhiles <hiles@hpe.com>
2020-03-03 09:32:14 -0500

    more go-like flow control

--- quoter.lua
+++ quoter.lua
@@ -15,12 +15,13 @@ function preRune(bp, r)
 	if bp.Buf.Settings["quoter.enable"] == false  then
 		return true
 	end
-	if bp.Cursor:HasSelection() then		
-		for i = 1, #quotePairs do
-			if r == quotePairs[i][1] or r == quotePairs[i][2] then
-				quote(bp, quotePairs[i][1], quotePairs[i][2])
-				return false
-			end
+	if bp.Cursor:HasSelection() == false then		
+		return true
+	end
+	for i = 1, #quotePairs do
+		if r == quotePairs[i][1] or r == quotePairs[i][2] then
+			quote(bp, quotePairs[i][1], quotePairs[i][2])
+			return false
 		end
 	end
 end
